93131f437aa2c5c3d4bd33378cc64663902500c9
zhangliguang <zhangliguang@lvye.com>
2019-01-08 13:52:59 +0800

    1.添加保险操作。。

---
+++ LvYeClubManage/ProductCode/LvyeProductWebRequest/LvYeHTTPClient+ClubInsurances.h
@@ -0,0 +1,18 @@
+//
+//  LvYeHTTPClient+ClubInsurances.h
+//  LvYeClubManage
+//
+//  Created by ZhangLiGuang on 2019/1/8.
+//  Copyright © 2019年 BJ_LY_SJ Technology Co.,Ltd. All rights reserved.
+//
+
+#import "LvYeHTTPClient.h"
+
+
+
+/**
+ 俱乐部保险信息内容
+ */
+@interface LvYeHTTPClient (ClubInsurances)
+
+@end
---
+++ LvYeClubManage/ProductCode/LvyeProductWebRequest/LvYeHTTPClient+ClubInsurances.m
@@ -0,0 +1,13 @@
+//
+//  LvYeHTTPClient+ClubInsurances.m
+//  LvYeClubManage
+//
+//  Created by ZhangLiGuang on 2019/1/8.
+//  Copyright © 2019年 BJ_LY_SJ Technology Co.,Ltd. All rights reserved.
+//
+
+#import "LvYeHTTPClient+ClubInsurances.h"
+
+@implementation LvYeHTTPClient (ClubInsurances)
+
+@end
